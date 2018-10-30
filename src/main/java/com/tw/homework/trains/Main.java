b0c9a1ef1bca4ff08cf19ca8ad38dc6f1a6c97b5
changwentao <changwenwen@126.com>
2018-10-30 09:55:49 +0800

    重构

--- src/main/java/com/tw/homework/trains/Main.java
+++ src/main/java/com/tw/homework/trains/Main.java
@@ -6,160 +6,6 @@ import java.util.*;
 
 public class Main {
 
-    /**
-     * 键值对方式直接获取确定路径的长度
-     *
-     * @param edges 包含起点终点权重的路径 [source, target, weight]
-     * @param route 确定的路径
-     * @return 返回确定路径的长度，如果不可达返回Integer.MAX_VALUE
-     */
-    private int certainRouteDistance(String[][] edges, String route) {
-        Map<String, Integer> vectorMap = new HashMap<>();
-        for (String[] edge : edges) {
-            vectorMap.put(edge[0] + edge[1], new Integer(edge[2]));
-        }
-        int distance = 0;
-        String[] routes = route.split("-");
-        for (int i = 1; i < routes.length; i++) {
-            String cur = routes[i - 1] + routes[i];
-            if (vectorMap.containsKey(cur)) {
-                distance += vectorMap.get(cur);
-            } else {
-                return Integer.MAX_VALUE;
-            }
-        }
-        return distance;
-    }
-
-    /**
-     * 迪杰斯特拉最小堆最短路径算法
-     *
-     * @param edges 包含起点终点权重的路径 [source, target, weight]
-     * @param s     要搜索的最短路径的起点
-     * @return 起点s可达的所有终点的最短路径
-     */
-    private Map<String, Integer> dijkstraShortestRoute(String[][] edges, String s) {
-        Map<String, List<TargetCity>> adjMap = new HashMap<>();
-        for (String[] edge : edges) {
-            adjMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(new TargetCity(edge[1], edge[2]));
-        }
-
-        Map<String, Integer> dist = new HashMap<>();
-        PriorityQueue<TargetCity> minHeap = new PriorityQueue<>(Comparator.comparingInt(TargetCity::getDistance));
-        minHeap.offer(new TargetCity(s, 0));
-
-        while (!minHeap.isEmpty()) {
-            TargetCity curr = minHeap.poll();
-            if (dist.containsKey(curr.getTo())) continue;
-            if (curr.getDistance() > 0) {
-                dist.put(curr.getTo(), curr.getDistance());
-            }
-            if (adjMap.containsKey(curr.getTo())) {
-                for (TargetCity edge : adjMap.get(curr.getTo())) {
-                    minHeap.offer(new TargetCity(edge.getTo(), edge.getDistance() + curr.getDistance()));
-                }
-            }
-        }
-
-        return dist;
-    }
-
-    /**
-     * 普通路径算法
-     *
-     * @param edges 包含起点终点权重的路径 [source, target, weight]
-     * @param s     要搜索的路径的起点
-     * @param e     要搜索的路径的起点
-     * @return 起点s达到e的所有路径，考虑环以及起点终点是自身
-     */
-    private List<RouteDistance> normalRoute(String[][] edges,
-                                            String s,
-                                            String e,
-                                            int stops, int maxDistance,
-                                            String conditionName) {
-        Map<String, List<TargetCity>> adjMap = new HashMap<>();
-        for (String[] edge : edges) {
-            adjMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(new TargetCity(edge[1], edge[2]));
-        }
-
-        List<RouteDistance> routeDistances = new ArrayList<>(16);
-        List<RouteDistance> tempRouteDistances = new ArrayList<>();
-
-        //起点
-        RouteDistance start = new RouteDistance();
-        start.addStop(new TargetCity(s, 0));
-        tempRouteDistances.add(start);
-
-        SearchStopConditionFactory factory = new SearchStopConditionFactory();
-        //递归遍历
-        while (!tempRouteDistances.isEmpty()) {
-            RouteDistance curr = tempRouteDistances.remove(tempRouteDistances.size()-1);
-            String lastStop = curr.getSb().substring(curr.getSb().length() - 1);
-            if (adjMap.containsKey(lastStop)) {
-                for (TargetCity edge : adjMap.get(lastStop)) {
-                    RouteDistance routeDistance = new RouteDistance();
-                    routeDistance.addStop(new TargetCity(curr.getSb().toString(), curr.getDistance()));
-
-                    TargetCity targetCity = new TargetCity(edge.getTo(), edge.getDistance());
-                    routeDistance.addStop(targetCity);
-                    SearchStopCondition condition = factory.getCondition(conditionName, routeDistance, stops, maxDistance);
-
-                    if (e.equals(edge.getTo())) {
-                        routeDistances.add(routeDistance);
-                    }
-
-                    if (!condition.canStop()) {
-                        tempRouteDistances.add(routeDistance);
-                    }
-                }
-            }
-        }
-
-        //最大步数直接返回
-        if (stops > 0 && ConditionConstants.MAXSTOPS.equals(conditionName)) {
-            return routeDistances;
-        }
-        //确定的步数将步数相等的过滤出来返回
-        List<RouteDistance> result = new ArrayList<>(16);
-        if (stops > 0 && ConditionConstants.EXACTLYSTOPS.equals(conditionName)) {
-            routeDistances.forEach(routeDistance -> {
-                if (routeDistance.getSb().length() - 1 == stops) {
-                    result.add(routeDistance);
-                }
-            });
-        }
-        //确定的最大距离的将距离符合的过滤出来返回
-        if (maxDistance > 0 && ConditionConstants.MAXDISTANCE.equals(conditionName)) {
-            routeDistances.forEach(routeDistance -> {
-                if (routeDistance.getDistance() < maxDistance) {
-                    result.add(routeDistance);
-                }
-            });
-        }
-        return result;
-    }
-
-
-    /**
-     * 输入数据检查 抛参数非法异常
-     *
-     * @param edges 包含起点终点权重的路径 [source, target, weight]
-     */
-    private void dataCheck(String[][] edges) {
-        Map<String, String> adjMap = new HashMap<>();
-        for (String[] edge : edges) {
-            if (edge[0].equals(edge[1])) {
-                throw new IllegalArgumentException("The given route has same start and end:" + edge[0] + edge[1]);
-            }
-            String key = edge[0] + edge[1];
-            if (adjMap.containsKey(key)) {
-                throw new IllegalArgumentException("The same route has appear more than once:" + key);
-            }
-            adjMap.put(edge[0] + edge[1], "TEMP");
-        }
-    }
-
-
     public static void main(String[] args) {
         //初始化录入数据
         String[][] edges = {
@@ -173,29 +19,28 @@ public class Main {
                 {"E", "B", "3"},
                 {"A", "E", "7"}
         };
-        Main main = new Main();
+        RouteSearchEngine engine = new RouteSearchEngine();
         //检查数据
-        main.dataCheck(edges);
+        engine.dataCheck(edges);
         //使用键值对的方式获取指定路径的距离
-        printDistance(1, main.certainRouteDistance(edges, "A-B-C"));
-        printDistance(2, main.certainRouteDistance(edges, "A-D"));
-        printDistance(3, main.certainRouteDistance(edges, "A-D-C"));
-        printDistance(4, main.certainRouteDistance(edges, "A-E-B-C-D"));
-        printDistance(5, main.certainRouteDistance(edges, "A-E-D"));
+        printDistance(1, engine.certainRouteDistance(edges, "A-B-C"));
+        printDistance(2, engine.certainRouteDistance(edges, "A-D"));
+        printDistance(3, engine.certainRouteDistance(edges, "A-D-C"));
+        printDistance(4, engine.certainRouteDistance(edges, "A-E-B-C-D"));
+        printDistance(5, engine.certainRouteDistance(edges, "A-E-D"));
 
         //非最短路径的遍历
-        printDistance(6, main.normalRoute(edges, "C", "C", 3, -1, ConditionConstants.MAXSTOPS).size());
-        printDistance(7, main.normalRoute(edges, "A", "C", 4, -1, ConditionConstants.EXACTLYSTOPS).size());
+        printDistance(6, engine.normalRoute(edges, "C", "C", 3, -1, ConditionConstants.MAXSTOPS).size());
+        printDistance(7, engine.normalRoute(edges, "A", "C", 4, -1, ConditionConstants.EXACTLYSTOPS).size());
 
         //迪杰斯特拉最小堆计算最短距离
-        Map<String, Integer> result8 = main.dijkstraShortestRoute(edges, "A");
+        Map<String, Integer> result8 = engine.dijkstraShortestRoute(edges, "A");
         printDistance(8, result8.get("C"));
-        Map<String, Integer> result9 = main.dijkstraShortestRoute(edges, "B");
+        Map<String, Integer> result9 = engine.dijkstraShortestRoute(edges, "B");
         printDistance(9, result9.get("B"));
 
         //非最短路径的遍历
-        printDistance(10, main.normalRoute(edges, "C", "C", -1, 30, ConditionConstants.MAXDISTANCE).size());
-
+        printDistance(10, engine.normalRoute(edges, "C", "C", -1, 30, ConditionConstants.MAXDISTANCE).size());
 
     }
 
---
+++ src/main/java/com/tw/homework/trains/RouteSearchEngine.java
@@ -0,0 +1,167 @@
+package com.tw.homework.trains;
+
+import com.tw.homework.trains.modle.*;
+
+import java.util.*;
+
+/**
+ * Function: 路径搜索类. <br>
+ * Date : 2018年10月30日 9:53 <br>
+ *
+ * @author : changwentao
+ */
+public class RouteSearchEngine {
+    /**
+     * 键值对方式直接获取确定路径的长度
+     *
+     * @param edges 包含起点终点权重的路径 [source, target, weight]
+     * @param route 确定的路径
+     * @return 返回确定路径的长度，如果不可达返回Integer.MAX_VALUE
+     */
+    public int certainRouteDistance(String[][] edges, String route) {
+        Map<String, Integer> vectorMap = new HashMap<>();
+        for (String[] edge : edges) {
+            vectorMap.put(edge[0] + edge[1], new Integer(edge[2]));
+        }
+        int distance = 0;
+        String[] routes = route.split("-");
+        for (int i = 1; i < routes.length; i++) {
+            String cur = routes[i - 1] + routes[i];
+            if (vectorMap.containsKey(cur)) {
+                distance += vectorMap.get(cur);
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+        return distance;
+    }
+
+    /**
+     * 迪杰斯特拉最小堆最短路径算法
+     *
+     * @param edges 包含起点终点权重的路径 [source, target, weight]
+     * @param s     要搜索的最短路径的起点
+     * @return 起点s可达的所有终点的最短路径
+     */
+    public Map<String, Integer> dijkstraShortestRoute(String[][] edges, String s) {
+        Map<String, List<TargetCity>> adjMap = new HashMap<>();
+        for (String[] edge : edges) {
+            adjMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(new TargetCity(edge[1], edge[2]));
+        }
+
+        Map<String, Integer> dist = new HashMap<>();
+        PriorityQueue<TargetCity> minHeap = new PriorityQueue<>(Comparator.comparingInt(TargetCity::getDistance));
+        minHeap.offer(new TargetCity(s, 0));
+
+        while (!minHeap.isEmpty()) {
+            TargetCity curr = minHeap.poll();
+            if (dist.containsKey(curr.getTo())) continue;
+            if (curr.getDistance() > 0) {
+                dist.put(curr.getTo(), curr.getDistance());
+            }
+            if (adjMap.containsKey(curr.getTo())) {
+                for (TargetCity edge : adjMap.get(curr.getTo())) {
+                    minHeap.offer(new TargetCity(edge.getTo(), edge.getDistance() + curr.getDistance()));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    /**
+     * 普通路径算法
+     *
+     * @param edges 包含起点终点权重的路径 [source, target, weight]
+     * @param s     要搜索的路径的起点
+     * @param e     要搜索的路径的起点
+     * @return 起点s达到e的所有路径，考虑环以及起点终点是自身
+     */
+    public List<RouteDistance> normalRoute(String[][] edges,
+                                            String s,
+                                            String e,
+                                            int stops, int maxDistance,
+                                            String conditionName) {
+        Map<String, List<TargetCity>> adjMap = new HashMap<>();
+        for (String[] edge : edges) {
+            adjMap.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(new TargetCity(edge[1], edge[2]));
+        }
+
+        List<RouteDistance> routeDistances = new ArrayList<>(16);
+        List<RouteDistance> tempRouteDistances = new ArrayList<>();
+
+        //起点
+        RouteDistance start = new RouteDistance();
+        start.addStop(new TargetCity(s, 0));
+        tempRouteDistances.add(start);
+
+        SearchStopConditionFactory factory = new SearchStopConditionFactory();
+        //递归遍历
+        while (!tempRouteDistances.isEmpty()) {
+            RouteDistance curr = tempRouteDistances.remove(tempRouteDistances.size()-1);
+            String lastStop = curr.getSb().substring(curr.getSb().length() - 1);
+            if (adjMap.containsKey(lastStop)) {
+                for (TargetCity edge : adjMap.get(lastStop)) {
+                    RouteDistance routeDistance = new RouteDistance();
+                    routeDistance.addStop(new TargetCity(curr.getSb().toString(), curr.getDistance()));
+
+                    TargetCity targetCity = new TargetCity(edge.getTo(), edge.getDistance());
+                    routeDistance.addStop(targetCity);
+                    SearchStopCondition condition = factory.getCondition(conditionName, routeDistance, stops, maxDistance);
+
+                    if (e.equals(edge.getTo())) {
+                        routeDistances.add(routeDistance);
+                    }
+
+                    if (!condition.canStop()) {
+                        tempRouteDistances.add(routeDistance);
+                    }
+                }
+            }
+        }
+
+        //最大步数直接返回
+        if (stops > 0 && ConditionConstants.MAXSTOPS.equals(conditionName)) {
+            return routeDistances;
+        }
+        //确定的步数将步数相等的过滤出来返回
+        List<RouteDistance> result = new ArrayList<>(16);
+        if (stops > 0 && ConditionConstants.EXACTLYSTOPS.equals(conditionName)) {
+            routeDistances.forEach(routeDistance -> {
+                if (routeDistance.getSb().length() - 1 == stops) {
+                    result.add(routeDistance);
+                }
+            });
+        }
+        //确定的最大距离的将距离符合的过滤出来返回
+        if (maxDistance > 0 && ConditionConstants.MAXDISTANCE.equals(conditionName)) {
+            routeDistances.forEach(routeDistance -> {
+                if (routeDistance.getDistance() < maxDistance) {
+                    result.add(routeDistance);
+                }
+            });
+        }
+        return result;
+    }
+
+
+    /**
+     * 输入数据检查 抛参数非法异常
+     *
+     * @param edges 包含起点终点权重的路径 [source, target, weight]
+     */
+    public void dataCheck(String[][] edges) {
+        Map<String, String> adjMap = new HashMap<>();
+        for (String[] edge : edges) {
+            if (edge[0].equals(edge[1])) {
+                throw new IllegalArgumentException("The given route has same start and end:" + edge[0] + edge[1]);
+            }
+            String key = edge[0] + edge[1];
+            if (adjMap.containsKey(key)) {
+                throw new IllegalArgumentException("The same route has appear more than once:" + key);
+            }
+            adjMap.put(edge[0] + edge[1], "TEMP");
+        }
+    }
+
+}
